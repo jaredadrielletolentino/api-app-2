063ca64d3d5d872aa1716d923a816c22357d5a30
jaredadrielletolentino <jaredadrielletolentino@gmail.com>
2025-07-15 22:31:25 +0800

    Updated backend api

--- models/Movie.js
+++ models/Movie.js
@@ -1,73 +1,39 @@
-const bcrypt = require('bcrypt');
-const User = require('../models/User');
-// const Enrollment = require("../models/Enrollment");
-const auth = require('../auth');
+const mongoose = require('mongoose');
 
+const commentSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    comment: {
+        type: String,
+        required: [true, 'Comment text is required']
+    }
+});
 
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Title is required']
+    },
+    director: {
+        type: String,
+        required: [true, 'Director is required']
+    },
+    year: {
+        type: Number,
+        required: [true, 'Year is required']
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required']
+    },
+    genre: {
+        type: String,
+        required: [true, 'Genre is required']
+    },
+    comments: [commentSchema]
+});
 
-// Register user
-module.exports.registerUser = (req, res) => {
-	if(!req.body.email.includes('@')) {
-		return res.status(200).send({message : 'Invalid email format'})
-	}
-
-	if(req.body.password.length < 8){
-		return res.status(200).send({ message: 'Password must be at least 8 characters' });
-	}
-
-	// Checking if email is already registered and if so, early exit
-	User.findOne({email : req.body.email })
-	.then(existingUser => {
-		if(existingUser){
-			return res.status(200).send({ message: 'Email Already Exists' });
-		}
-	
-
-		const newUser = new User({
-			email : req.body.email,
-			password : bcrypt.hashSync(req.body.password, 10)
-		});
-
-		return newUser.save()
-		.then(user => {
-			res.status(201).send({
-				message : 'Successfully Registered'
-			});
-		});
-	})
-	.catch(err => auth.errorHandler(err, req, res));
-}
-
-// Login
-module.exports.loginUser = (req, res) => {
-	User.findOne({ email: req.body.email })
-		.then(user => {
-			if (!user) {
-				return res.status(200).send({ message: 'User not found' });
-			}
-
-			const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.password);
-
-			if (!isPasswordCorrect) {
-				return res.status(200).send({ message: 'Incorrect password' });
-			}
-
-			res.status(200).send({
-				access: auth.createAccessToken(user)
-			});
-		})
-		.catch(err => auth.errorHandler(err, req, res));
-};
-
-// Get user details (profile)
-module.exports.getProfile = (req, res) => {
-  return User.findById(req.user.id)
-    .select('-password')
-    .then(user => {
-      if (!user) {
-        return res.status(200).send({ message: 'User not found' });
-      }
-      res.status(200).send(user);
-    })
-    .catch(error => auth.errorHandler(error, req, res)); // make sure `auth` is imported
-};
+module.exports = mongoose.model('Movie', movieSchema);
--- models/User.js
+++ models/User.js
@@ -1,25 +1,20 @@
-//[Section] Activity
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    
-    email: {
-        type: String,
-        required: [true, 'Email is Required']
-    },
-    password: {
-        type: String,
-        required: [true, 'Password is Required']
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    },
-    mobileNo: {
-        type: String,
-        required: [true, 'Mobile Number is Required']
-    }
-});
 
+	email : {
+		type: String,
+		required : [true, 'Email is Required']
+	},
+	password : {
+		type: String,
+		required : [true, 'Password is Required']
+	},
+	isAdmin : {
+		type : Boolean,
+		default : false
+	}
+
+})
 
 module.exports = mongoose.model('User', userSchema);
